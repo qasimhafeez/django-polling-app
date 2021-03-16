c46d61f53c08c1775b27749a5b75c840b12851b7
Qasim Hafeez <qasimh556@gmail.com>
2021-03-16 17:19:39 +0400

    dynamic view using template

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from .models import Question, Choice
+from django.http import Http404
 
 # Create your views here.
 
 def index(req):
-    return render(req, "polls/index.html")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(req, "polls/index.html", context)
+
+def detail(req, question_id):
+    try:
+        question = Question.objects.get(pk=question.id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists")
+    return render(req, 'polls/result.html', {"question": question})
