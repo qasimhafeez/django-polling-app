dfffed45d2ed40a1b252f8e98d391f8d367e4d37
Qasim Hafeez <qasimh556@gmail.com>
2021-03-17 13:12:03 +0400

    vote url added

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question, Choice
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -11,7 +12,22 @@ def index(req):
 
 def detail(req, question_id):
     try:
-        question = Question.objects.get(pk=question.id)
+        question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exists")
-    return render(req, 'polls/result.html', {"question": question})
+    return render(req, 'polls/detail.html', {"question": question})
+
+def results(req, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(req, "polls/results.html", {"question": question})
+
+def vote(req, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=req.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(req, "polls/detail.html", {"question": question, "error_message": "Please select a choice", })
+    else:
+        selected_choice.vote += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
